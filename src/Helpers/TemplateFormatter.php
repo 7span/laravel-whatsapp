de0a077e957ea443f4d30e962c4039df505277e5
Kajal Pandya <kajal@7span.com>
2022-06-16 18:02:07 +0530

    ds store file removed

--- src/Helpers/TemplateFormatter.php
+++ src/Helpers/TemplateFormatter.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace SevenSpan\ValueFirst\Helpers;
 
 class TemplateFormatter
